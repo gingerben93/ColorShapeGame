7d9e1be45665b104b0e8500cdda731a95b50976c
gingerben93 <gingerben93@gmail.com>
2018-07-22 01:45:57 -0700

    First Upload
    New Color Shape game.

---
+++ ColorShapes/Assets/Scripts/GameController.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public List<GameObject> buttons = new List<GameObject>();
+
+    public List<Player> Players = new List<Player>();
+
+    public struct Player
+    {
+        public HashSet<Vector2> playerVisited;
+        public List<Vector2> playerReachable;
+        public Queue<Vector2> playerQ;
+
+        public Player(HashSet<Vector2> pv, List<Vector2> pr, Queue<Vector2> pq)
+        {
+            playerVisited = pv;
+            playerReachable = pr;
+            playerQ = pq;
+        }
+    }
+
+    public void AddPlayer()
+    {
+        HashSet<Vector2> pv = new HashSet<Vector2>();
+        List<Vector2> pr = new List<Vector2>();
+        Queue<Vector2> pq = new Queue<Vector2>();
+        Player newPlayer = new Player(pv, pr, pq);
+        Players.Add(newPlayer);
+    }
+
+    int turn = 0;
+
+    public static GameController GameControllerSingle;
+
+    //create singleton of class
+    void Awake()
+    {
+        if (GameControllerSingle == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GameControllerSingle = this;
+        }
+        else if (GameControllerSingle != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(turn != -1)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                ButtonClick(Color.red, Players[turn].playerVisited, Players[turn].playerReachable, Players[turn].playerQ);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                ButtonClick(Color.yellow, Players[turn].playerVisited, Players[turn].playerReachable, Players[turn].playerQ);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                ButtonClick(Color.green, Players[turn].playerVisited, Players[turn].playerReachable, Players[turn].playerQ);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                ButtonClick(Color.cyan, Players[turn].playerVisited, Players[turn].playerReachable, Players[turn].playerQ);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                ButtonClick(Color.blue, Players[turn].playerVisited, Players[turn].playerReachable, Players[turn].playerQ);
+            }
+        }
+    }
+
+    void ButtonClick(Color buttonColor, HashSet<Vector2> visited, List<Vector2> reachable, Queue<Vector2> playerQ)
+    {
+        List<Vector2> tempReach = new List<Vector2>();
+
+        //check all reachable squares for correct color
+        foreach (Vector2 vec in reachable)
+        {
+            //or black for first square
+            if (GenerateGame.GenerateGameSingle.gameBoard[(int)vec.x, (int)vec.y].GetComponent<SpriteRenderer>().color == buttonColor || GenerateGame.GenerateGameSingle.gameBoard[(int)vec.x, (int)vec.y].GetComponent<SpriteRenderer>().color == Color.black)
+            {
+                tempReach.Add(vec);
+                visited.Add(vec);
+            }
+        }
+
+        //place correct color squares in queue
+        foreach (Vector2 vec in tempReach)
+        {
+            playerQ.Enqueue(vec);
+        }
+
+        //check all correct color squares for their neighbors that are same color //dijstras
+        while (playerQ.Count != 0)
+        {
+            Vector2 currentSquare = playerQ.Dequeue();
+            Vector2 temp;
+
+            //right
+            if (currentSquare.x + 1 < GenerateGame.GenerateGameSingle.width)
+            {
+                temp = new Vector2(currentSquare.x + 1, currentSquare.y);
+                //color correct
+                if (GenerateGame.GenerateGameSingle.gameBoard[(int)currentSquare.x + 1, (int)currentSquare.y].GetComponent<SpriteRenderer>().color == buttonColor)
+                {
+                    //if not in visited then put there
+                    if (!visited.Contains(temp))
+                    {
+                        playerQ.Enqueue(temp);
+                        visited.Add(temp);
+                        reachable.Remove(temp);
+                    }
+                }
+                //if color not correct add to reachable 
+                else if (!reachable.Contains(temp))
+                {
+                    reachable.Add(temp);
+                }
+            }
+
+            //left
+            if (currentSquare.x - 1 >= 0)
+            {
+                temp = new Vector2(currentSquare.x - 1, currentSquare.y);
+                if (GenerateGame.GenerateGameSingle.gameBoard[(int)currentSquare.x - 1, (int)currentSquare.y].GetComponent<SpriteRenderer>().color == buttonColor)
+                {
+                    if (!visited.Contains(temp))
+                    {
+                        playerQ.Enqueue(temp);
+                        visited.Add(temp);
+                        reachable.Remove(temp);
+                    }
+                }
+                else if (!reachable.Contains(temp))
+                {
+                    reachable.Add(temp);
+                }
+            }
+
+            //up
+            if (currentSquare.y + 1 < GenerateGame.GenerateGameSingle.height)
+            {
+                temp = new Vector2(currentSquare.x, currentSquare.y + 1);
+                if (GenerateGame.GenerateGameSingle.gameBoard[(int)currentSquare.x, (int)currentSquare.y + 1].GetComponent<SpriteRenderer>().color == buttonColor)
+                {
+                    if (!visited.Contains(temp))
+                    {
+                        playerQ.Enqueue(temp);
+                        visited.Add(temp);
+                        reachable.Remove(temp);
+                    }
+                }
+                else if (!reachable.Contains(temp))
+                {
+                    reachable.Add(temp);
+                }
+            }
+
+            //down
+            if (currentSquare.y - 1 >= 0)
+            {
+                temp = new Vector2(currentSquare.x, currentSquare.y - 1);
+                if (GenerateGame.GenerateGameSingle.gameBoard[(int)currentSquare.x, (int)currentSquare.y - 1].GetComponent<SpriteRenderer>().color == buttonColor)
+                {
+                    if (!visited.Contains(temp))
+                    {
+                        playerQ.Enqueue(temp);
+                        visited.Add(temp);
+                        reachable.Remove(temp);
+                    }
+                }
+                else if (!reachable.Contains(temp))
+                {
+                    reachable.Add(temp);
+                }
+            }
+        }
+
+        //change color all squares player owns
+        foreach (Vector2 vec in visited)
+        {
+            GenerateGame.GenerateGameSingle.gameBoard[(int)vec.x, (int)vec.y].GetComponent<SpriteRenderer>().color = buttonColor;
+        }
+
+        //next turn
+        turn += 1;
+        turn = turn % GenerateGame.GenerateGameSingle.numPlayers;
+    }
+}
---
+++ ColorShapes/Assets/Scripts/GenerateGame.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGame : MonoBehaviour {
+
+    public GameObject[,] gameBoard;
+    public int width;
+    public int height;
+
+    public int numberColors;
+    public int numPlayers;
+
+    private List<Vector2> startLocations = new List<Vector2>();
+
+    public GameObject BoxPrefab;
+
+    public static GenerateGame GenerateGameSingle;
+    //create singleton of class
+    void Awake()
+    {
+        if (GenerateGameSingle == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GenerateGameSingle = this;
+        }
+        else if (GenerateGameSingle != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        //start variables
+        width = 50;
+        height = 10;
+        numberColors = 5;
+        numPlayers = 2;
+        gameBoard = new GameObject[width, height];
+
+        StartGameGeneration();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		
+	}
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void StartGameGeneration()
+    {
+        //makes grid; Instantiate color square
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int color = Random.Range(0, numberColors);
+                
+                var tempBox = Instantiate(BoxPrefab);
+                gameBoard[x, y] = tempBox;
+                tempBox.GetComponent<SpriteRenderer>().color = PickColor(color);
+                tempBox.transform.position = new Vector3(x - width / 2, y - height / 2, 0);
+
+                //hexagon pattern
+                //if (x % 2 == 1)
+                //    tempBox.transform.position = new Vector3(x - width / 2, y - height / 2 + .25f, 0);
+                //else
+                //    tempBox.transform.position = new Vector3(x - width / 2, y - height / 2 - .25f, 0);
+
+            }
+        }
+
+        //make player Buttons
+        GameObject ColorButtonPanel = GameObject.Find("ColorButtonPanel");
+        for (int y = 0; y < numberColors; y++)
+        {
+            var tempBox = Instantiate(BoxPrefab, ColorButtonPanel.transform);
+            GameController.GameControllerSingle.buttons.Add(tempBox);
+            tempBox.GetComponent<SpriteRenderer>().color = PickColor(y);
+            tempBox.GetComponent<SpriteRenderer>().size = new Vector2(4, 1);
+            //set position from panel not world
+            tempBox.transform.localPosition = new Vector3(0, ColorButtonPanel.GetComponent<SpriteRenderer>().size.y / 2f - .5f - y, 0);
+        }
+
+        StartConitions();
+    }
+
+    private void StartConitions()
+    {
+        //possible player starts
+        startLocations.Add(new Vector2(0, 0));
+        startLocations.Add(new Vector2(width - 1, height - 1));
+        startLocations.Add(new Vector2(0, height - 1));
+        startLocations.Add(new Vector2(width - 1, 0));
+
+        for (int x = 0; x < numPlayers; x++)
+        {
+            int ranStart = Random.Range(0, startLocations.Count);
+
+            GameController.GameControllerSingle.AddPlayer();
+            GameController.GameControllerSingle.Players[x].playerReachable.Add(startLocations[ranStart]);
+            gameBoard[(int)startLocations[ranStart].x, (int)startLocations[ranStart].y].GetComponent<SpriteRenderer>().color = Color.black;
+            //stop player from being in same spot
+            startLocations.RemoveAt(ranStart);
+        }
+    }
+
+    public Color PickColor(int color)
+    {
+        switch (color)
+        {
+            case 0:
+                return Color.red;
+            case 1:
+                return Color.yellow;
+            case 2:
+                return Color.green;
+            case 3:
+                return Color.cyan;
+            case 4:
+                return Color.blue;
+            case 5:
+                return Color.magenta;
+            case 6:
+                return Color.white;
+            default:
+                return Color.black;
+        }
+    }
+
+}
